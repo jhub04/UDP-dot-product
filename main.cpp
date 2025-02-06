54abd798f7698e6354d5d9b6f108aa400e0ea91b
jonathan <jonathhu@stud.ntnu.no>
2025-02-06 14:58:59 +0100

    Dot product calculation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int dotProduct(vector<int> v1, vector<int> v2) {
+    int result = 0;
+    for (int i = 0; i < v1.size(); i++) {
+        result += v1[i] * v2[i];
+    }
+
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> v1 = {2, 3, 1};
+    vector<int> v2 = {4, 2, 5};
+
+    cout << dotProduct(v1, v2) << endl;
+
     return 0;
 }
